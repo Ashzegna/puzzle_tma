7e5c086b0d478e7b3a5ce03ffd54d91a5ef016ab
Ashzegna <zegnalab@gmail.com>
2025-03-22 21:58:12 +0300

    Update and rename index.html to src/index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import WebApp from '@twa-dev/sdk';
+
+// Инициализация Telegram WebApp
+WebApp.ready();
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
