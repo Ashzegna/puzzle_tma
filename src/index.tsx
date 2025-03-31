0ddfa72513733319d9217b52d4f839d56ac4e6cb
Ashzegna <zegnalab@gmail.com>
2025-03-31 19:17:18 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './animations.css';
-import './ios-styles.css'; // Добавлен импорт новых стилей
+import './ios-styles.css';
 import App from './App';
 import WebApp from '@twa-dev/sdk';
+
+// Правильное имя бота для всего приложения
+export const BOT_USERNAME = 'knowyourmagic_bot';
 
 // Инициализация Telegram WebApp
 WebApp.ready();
